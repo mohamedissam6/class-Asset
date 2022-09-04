6c1e66373a35a20ed4c6e439fbef25646460d829
mohamedissam6 <112808440+mohamedissam6@users.noreply.github.com>
2022-09-04 20:24:07 +0200

    Add files via upload

---
+++ question1/question1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+    internal class Asset
+    {
+        public string Name { get; set; }
+        public decimal Value;
+        public decimal DepreciationRate;
+        
+
+        public Asset(string name, decimal value, decimal depreciationRate)
+        {
+            Name = name;
+            Value = value;
+            DepreciationRate = depreciationRate;
+        }
+        
+        public decimal value
+        {
+            set
+            {
+                if (value >= 0) Value = value;
+            }
+            get
+            {
+                return Value;   
+            }
+        }
+        public decimal deperciationRate
+        {
+            set
+            {
+                if (value >= 0) DepreciationRate = value;
+            }
+            get
+            {
+                return Value;
+            }
+        }
+    }
+}
---
+++ question1/question1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Asset asset1 = new Asset("mohamed", -22, 25);
+            Asset asset2 = new Asset("essam", 200, 30);
+            Console.WriteLine("name: {0}\tvalue: {1}\tdepreciation rate: {2}", asset1.Name, asset1.Value, asset1.DepreciationRate);
+            Console.WriteLine("name: {0}\tvalue: {1}\tdepreciation rate: {2}", asset2.Name, asset2.Value, asset2.DepreciationRate);
+            Console.WriteLine("\n5% increase\n");
+            asset1.Value = asset1.Value + (Convert.ToDecimal(Convert.ToDouble(asset1.Value) * (0.05)));
+            asset2.Value = asset2.Value + (Convert.ToDecimal(Convert.ToDouble(asset2.Value) * (0.05)));
+            Console.WriteLine("name: {0}\tvalue: {1}\tdepreciation rate: {2}", asset1.Name, asset1.Value, asset1.DepreciationRate);
+            Console.WriteLine("name: {0}\tvalue: {1}\tdepreciation rate: {2}", asset2.Name, asset2.Value, asset2.DepreciationRate);
+            Console.ReadLine();
+
+        }
+    }
+}
